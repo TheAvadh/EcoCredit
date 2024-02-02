90d55f7d28b134634b1c78bddf49a11b09498c0d
Raisa Putri <rs439226@dal.ca>
2024-02-02 14:44:06 -0400

    add skeleton code

---
+++ src/main/java/com/group1/ecocredit/EcocreditApplication.java
@@ -0,0 +1,13 @@
+package com.group1.ecocredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocreditApplication.class, args);
+	}
+
+}
