2d0a513073c1870e579467a84e71ab642533e918
Geerthana Kanagalingame <gr964904@dal.ca>
2024-02-06 13:06:00 -0400

    enable userService to update profile

--- src/main/java/com/group1/ecocredit/services/UserService.java
+++ src/main/java/com/group1/ecocredit/services/UserService.java
@@ -1,8 +1,13 @@
 package com.group1.ecocredit.services;
 
+import com.group1.ecocredit.dto.UpdateProfileRequest;
+import com.group1.ecocredit.dto.UpdateProfileResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService {
     UserDetailsService userDetailsService();
+
+    UpdateProfileResponse updateProfile(UpdateProfileRequest updateProfileRequest);
 
 }
