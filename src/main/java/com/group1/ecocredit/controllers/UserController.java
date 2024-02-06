435a7f51db152365d18183e655665b6d5bd4243a
Geerthana Kanagalingame <gr964904@dal.ca>
2024-02-06 13:01:15 -0400

    added user profile update functionality

--- src/main/java/com/group1/ecocredit/controllers/UserController.java
+++ src/main/java/com/group1/ecocredit/controllers/UserController.java
@@ -1,18 +1,27 @@
 package com.group1.ecocredit.controllers;
 
+import com.group1.ecocredit.dto.SignUpRequest;
+import com.group1.ecocredit.dto.UpdateProfileRequest;
+import com.group1.ecocredit.dto.UpdateProfileResponse;
+import com.group1.ecocredit.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 
 public class UserController {
+
+    private final UserService userService;
     @GetMapping
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hi user");
+    }
+
+    @PutMapping("/updateProfile")
+    public ResponseEntity<UpdateProfileResponse> updateProfile(@RequestBody UpdateProfileRequest updateProfileRequest) {
+        return ResponseEntity.ok(userService.updateProfile(updateProfileRequest));
     }
 }
