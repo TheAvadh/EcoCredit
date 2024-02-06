cb4c20b9a2bdb5df82b0541fa450a2220c94acd2
Geerthana Kanagalingame <gr964904@dal.ca>
2024-02-06 13:05:01 -0400

    add to find profile by id

--- src/main/java/com/group1/ecocredit/repositories/UserRepository.java
+++ src/main/java/com/group1/ecocredit/repositories/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
+    Optional<User> findById(Integer id);
+
     User findByRole(Role role);
 
 }
