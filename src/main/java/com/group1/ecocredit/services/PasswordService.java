a007d1e60ad34529493302d436dad8ddac5952b0
Ajay Nair <ajaypnair99@gmail.com>
2024-02-08 10:08:59 -0400

    Removed application.properties from .gitignore
    Signed-off-by: Ajay Nair <ajaypnair99@gmail.com>

--- src/main/java/com/group1/ecocredit/services/PasswordService.java
+++ src/main/java/com/group1/ecocredit/services/PasswordService.java
@@ -2,8 +2,6 @@ package com.group1.ecocredit.services;
 
 import com.group1.ecocredit.dto.ForgetPasswordRequest;
 import com.group1.ecocredit.dto.PasswordResetRequest;
-import com.group1.ecocredit.models.PasswordResetToken;
-import com.group1.ecocredit.models.User;
 
 public interface PasswordService {
     boolean forgetPassword(ForgetPasswordRequest request);
