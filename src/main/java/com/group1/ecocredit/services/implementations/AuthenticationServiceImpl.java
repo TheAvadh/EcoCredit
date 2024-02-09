6b66632045e8f9f97a5aec85b8a5836169427258
Geerthana Kanagalingame <gr964904@dal.ca>
2024-02-08 22:38:52 -0400

    fix: modification based on review comments

--- src/main/java/com/group1/ecocredit/services/implementations/AuthenticationServiceImpl.java
+++ src/main/java/com/group1/ecocredit/services/implementations/AuthenticationServiceImpl.java
@@ -32,7 +32,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         user.setEmail(signUpRequest.getEmail());
         user.setFirstName(signUpRequest.getFirstName());
         user.setLastName(signUpRequest.getLastName());
-
         user.setRole(Role.USER);
         //  encrypt raw password to hash password
         user.setPassword(passwordEncoder.encode(signUpRequest.getPassword()));
