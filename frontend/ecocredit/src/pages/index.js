384718ed550051d400bae684429ad2e4fdf6843c
Christin Saji <ch580848@dal.ca>
2024-02-20 01:18:14 -0400

    add role selection page

--- frontend/ecocredit/src/pages/index.js
+++ frontend/ecocredit/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LoginSignup } from "./LoginSignup/LoginSignup";
 export { default as ForgetPassword } from "./ForgetPassword/ForgetPassword";
 export { default as ResetPassword } from "./ResetPassword/ResetPassword";
+export { default as RoleSelection } from "./RoleSelection/RoleSelection";
