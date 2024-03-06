d7d8aa19ce85517091d615900415d1adcc8bcbdc
Christin Saji <ch580848@dal.ca>
2024-03-06 19:42:51 -0400

    add background image

--- frontend/ecocredit/src/components/PickupScheduleContainer/PickupScheduleContainer.js
+++ frontend/ecocredit/src/components/PickupScheduleContainer/PickupScheduleContainer.js
@@ -2,12 +2,16 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./PickupScheduleContainer.css";
 
 const PickupScheduleContainer = ({ children, title }) => {
   return (
-    <Container className="d-flex vh-100 justify-content-center align-items-center">
+    <Container
+      fluid
+      className="d-flex vh-100 justify-content-center align-items-center background-image"
+    >
       <Row className="w-100">
-        <Col lg={6} className="mx-auto">
+        <Col lg={4} className="mx-auto">
           <div className="form-container bg-ec-grey">
             <h2 className="text-center text-ec-dark-green mb-4">{title}</h2>
             {children}
