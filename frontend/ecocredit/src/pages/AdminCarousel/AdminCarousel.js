9e762a476ec02f0cf147dca6303fd1fcc6d171bb
Raisa Putri <rs439226@dal.ca>
2024-03-10 21:12:39 -0300

    add admin dashboard carousel

---
+++ frontend/ecocredit/src/pages/AdminCarousel/AdminCarousel.js
@@ -0,0 +1,40 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+const AdminCarousel = () => {
+    return (
+        <Carousel>
+        <Carousel.Item interval={1000}>
+          <a href="/view-pickups">
+            <img src='https://img.pikbest.com/wp/202405/recycle-bin-assorted-waste-on-eco-friendly-backdrop-recycling-symbol-and-3d-rendered-rubbish_9831528.jpg!sw800' 
+            className='d-block w-100'
+                alt='trash pickup'/>
+          </a>
+          <Carousel.Caption>
+            <h3>View Pickups</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <a href="/view-bids">
+            <img src='https://freestar.com/wp-content/uploads/2023/06/How-Does-In-App-Header-Bidding-Work-Matt.png'  
+            className = 'd-block w-100' 
+            alt='bids' />
+          </a>
+          <Carousel.Caption>
+            <h3>View bids</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1000}>
+          <a href="/create-a-bid">
+            <img src='https://media.istockphoto.com/id/1319023983/photo/announcement-concept-cheerful-black-woman-shouting-with-megaphone-in-hands-blue-background.jpg?s=612x612&w=0&k=20&c=SF-qv1Saei8cgKapMAnwqeYM8FftgMDt-eepUi4Nd_E=' 
+            className = 'd-block w-100'
+            alt='create-bid' />
+          </a>
+          <Carousel.Caption>
+            <h3>Create a Bid</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+};
+
+export default AdminCarousel;
---
+++ frontend/ecocredit/src/pages/AdminDashboard/AdminDashboard.js
@@ -0,0 +1,11 @@
+import AdminCarousel from "../AdminCarousel/AdminCarousel";
+
+const AdminDashboard = () => {
+    return (
+        <div>
+            <AdminCarousel/>
+        </div>
+    );
+};
+
+export default AdminDashboard;
