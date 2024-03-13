3366873ed844a75990db4d8a656bd7fb2c787d14
Christin Saji <ch580848@dal.ca>
2024-03-12 23:17:29 -0300

    add GET API functionality and updated background image

--- frontend/ecocredit/src/pages/PickupStatus/PickupStatus.js
+++ frontend/ecocredit/src/pages/PickupStatus/PickupStatus.js
@@ -1,15 +1,40 @@
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Table from "react-bootstrap/Table";
+import Cookies from "js-cookie";
 import "../../components/PickupScheduleContainer/PickupScheduleContainer.css";
 
 const PickupStatus = () => {
+  const [pickups, setPickups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_BASE_URL}/pickups/${localStorage.getItem(
+        "userId"
+      )}`,
+      {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Authorization: `Bearer ${Cookies.get("token")}`,
+          Accept: "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setPickups(data))
+      .catch((error) =>
+        console.error("Fetching pickup status data failed:", error)
+      );
+  }, []);
+
   return (
     <Container fluid className="background-image">
       <Row className="d-flex justify-content-center align-items-center">
         <Col lg={6} className="mt-5">
-          <div className="shadow-lg rounded-4 bg-ec-grey p-3">
+          <div className="shadow-lg rounded-4 bg-ec-grey p-4">
             <h1 className="text-center text-ec-dark-green p-2">
               Pickup Status
             </h1>
@@ -17,7 +42,7 @@ const PickupStatus = () => {
               striped
               bordered
               hover
-              variant="ec-light-green"
+              variant="ec-grey"
               className="table-custom mb-0"
             >
               <thead>
@@ -29,24 +54,14 @@ const PickupStatus = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>2024-03-08</td>
-                  <td>12:30</td>
-                  <td>SCHEDULED</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>2024-03-07</td>
-                  <td>10:15</td>
-                  <td>IN-PROGRESS</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>2023-02-29</td>
-                  <td>15:45</td>
-                  <td>CANCELED</td>
-                </tr>
+                {pickups.map((pickup) => (
+                  <tr key={pickup.pickupId}>
+                    <td>{pickup.pickupId}</td>
+                    <td>{pickup.pickupDate}</td>
+                    <td>{pickup.pickupTime}</td>
+                    <td>{pickup.pickupStatus}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
           </div>
