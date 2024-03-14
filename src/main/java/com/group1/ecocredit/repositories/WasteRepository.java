0a3ed2391fe9c717724d5fbf3e0508c453ceb1c5
TheAvadh <avadhrakholiya@gmail.com>
2024-03-13 23:20:38 -0300

    feat-bid-init

--- src/main/java/com/group1/ecocredit/repositories/WasteRepository.java
+++ src/main/java/com/group1/ecocredit/repositories/WasteRepository.java
@@ -2,6 +2,12 @@ package com.group1.ecocredit.repositories;
 
 import com.group1.ecocredit.models.Waste;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface WasteRepository extends JpaRepository<Waste, Long> {
+
+    Optional<Waste> findById(Long wasteId);
 }
--- src/main/java/com/group1/ecocredit/services/BidService.java
+++ src/main/java/com/group1/ecocredit/services/BidService.java
@@ -3,4 +3,5 @@ package com.group1.ecocredit.services;
 public interface BidService {
 
     void putWasteForBid(Long wasteId);
+
 }
