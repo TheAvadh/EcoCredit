7fdf16395876e7b870e5435101f1d574e45016f1
Christin Saji <ch580848@dal.ca>
2024-03-17 11:50:50 -0300

    add recycler dashboard

--- frontend/ecocredit/src/pages/index.js
+++ frontend/ecocredit/src/pages/index.js
@@ -3,4 +3,5 @@ export { default as ForgetPassword } from "./ForgetPassword/ForgetPassword";
 export { default as ResetPassword } from "./ResetPassword/ResetPassword";
 export { default as RoleSelection } from "./RoleSelection/RoleSelection";
 export { default as CustomerDashboard } from "./CustomerDashboard/CustomerDashboard";
+export { default as RecyclerDashboard } from "./RecyclerDashboard/RecyclerDashboard";
 export { default as ProfilePage } from "./ProfilePage/ProfilePage";
