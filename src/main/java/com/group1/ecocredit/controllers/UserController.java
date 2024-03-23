6bf343328e5b2cc880c2e9e3db820961c381d2bb
Raisa Putri <rs439226@dal.ca>
2024-03-23 17:39:58 -0300

    write tests for user controller

--- src/main/java/com/group1/ecocredit/controllers/UserController.java
+++ src/main/java/com/group1/ecocredit/controllers/UserController.java
@@ -23,7 +23,9 @@ public class UserController {
     private final UserService userService;
 
     private final JWTService jwtService;
-    
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
     @PutMapping("/update-profile")
     public ResponseEntity<?> updateProfile(
             @RequestBody UpdateProfileRequest updateProfileRequest) {
@@ -50,7 +52,9 @@ public class UserController {
         if (!authentication.isAuthenticated()) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
-        Integer userId = Integer.parseInt(jwtService.extractUserID(request.getHeader("Authorization")));
+
+        String requestHeader = request.getHeader(AUTHORIZATION_HEADER);
+        Integer userId = Integer.parseInt(jwtService.extractUserID(requestHeader));
 
         UserDetailsResponse userDTO = userService.getUserById(userId);
         if (userDTO != null) {
