27e04ed3d7fc78f0d33e14b358982a674b3428ca
Raisa Putri <rs439226@dal.ca>
2024-03-25 20:25:05 -0300

    add explanatory comment on pickup status constant class

--- src/main/java/com/group1/ecocredit/models/PickupStatus.java
+++ src/main/java/com/group1/ecocredit/models/PickupStatus.java
@@ -1,7 +1,9 @@
 package com.group1.ecocredit.models;
 
 public final class PickupStatus {
-    private PickupStatus() {}
+    private PickupStatus() {
+        // To prevent anyone from constructing objects of this class
+    }
     public static String SCHEDULED = "SCHEDULED";
     public static String IN_PROGRESS = "IN_PROGRESS";
     public static String COMPLETED = "COMPLETED";
