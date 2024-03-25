dc37a7d670dea74924addab861962427f231e486
Raisa Putri <rs439226@dal.ca>
2024-03-25 20:10:08 -0300

    make pickup status constants class final

--- src/main/java/com/group1/ecocredit/models/PickupStatus.java
+++ src/main/java/com/group1/ecocredit/models/PickupStatus.java
@@ -1,10 +1,9 @@
 package com.group1.ecocredit.models;
 
-public class PickupStatus {
+public final class PickupStatus {
     public static String SCHEDULED = "SCHEDULED";
     public static String IN_PROGRESS = "IN_PROGRESS";
     public static String COMPLETED = "COMPLETED";
     public static String CANCELED = "CANCELED";
-
     public static String AWAITING_PAYMENT = "AWAITING_PAYMENT";
 }
