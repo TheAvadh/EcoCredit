ff5e0d360563890f0029fca0d6fadea5308d7819
Raisa Putri <rs439226@dal.ca>
2024-03-27 11:47:21 -0300

    refactor price mapper svc

--- src/main/java/com/group1/ecocredit/services/implementations/CategoryPriceServiceImpl.java
+++ src/main/java/com/group1/ecocredit/services/implementations/CategoryPriceServiceImpl.java
@@ -6,6 +6,7 @@ import com.group1.ecocredit.services.CategoryPriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,5 +28,10 @@ public class CategoryPriceServiceImpl implements CategoryPriceService {
     @Override
     public CategoryPrice save(CategoryPrice categoryPrice) {
         return categoryPriceRepository.save(categoryPrice);
+    }
+
+    @Override
+    public List<CategoryPrice> findAll() {
+        return categoryPriceRepository.findAll();
     }
 }
