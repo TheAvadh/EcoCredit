d74d24b766597bf349cfe40580d4cb9da2c458f4
bhavyadave <bh392017@dal.ca>
2024-03-27 10:32:23 -0300

    refactor for build failure

--- src/test/java/com/group1/ecocredit/CreditConversionServiceTests.java
+++ src/test/java/com/group1/ecocredit/CreditConversionServiceTests.java
@@ -16,32 +16,32 @@ public class CreditConversionServiceTests {
     }
     @Test
     void convert_CADtoCAD_ShouldReturnSameAmount() {
-        double converted = creditConversionService.convert(100L, Currency.CAD);
+        double converted = creditConversionService.convert(100.0, Currency.CAD);
         assertEquals(100.0, converted);
     }
 
     @Test
     void convert_CADtoEUR_ShouldReturnCorrectAmount() {
-        double converted = creditConversionService.convert(100L, Currency.EUR);
+        double converted = creditConversionService.convert(100.0, Currency.EUR);
         assertEquals(150.0, converted);
     }
 
     @Test
     void convert_CADtoUSD_ShouldReturnCorrectAmount() {
-        double converted = creditConversionService.convert(100L, Currency.USD);
+        double converted = creditConversionService.convert(100.0, Currency.USD);
         assertEquals(121.0, converted);
     }
 
     @Test
     void convert_CADtoJPY_ShouldReturnCorrectAmount() {
-        double converted = creditConversionService.convert(100L, Currency.JPY);
+        double converted = creditConversionService.convert(100.0, Currency.JPY);
         assertEquals(70.0, converted);
     }
 
     @Test
     void convert_WithRounding_ShouldReturnCeiledAmount() {
         creditConversionService.conversionRates.put(Currency.EUR, 1.55f);
-        double converted = creditConversionService.convert(50L, Currency.EUR);
+        double converted = creditConversionService.convert(50.0, Currency.EUR);
         assertEquals(78.0, converted);
     }
 
