ee5ac8c1b631c194890decac12c59d81c90829e5
Ajaykumar Premkumar Nair <aj812411@dal.ca>
2024-03-28 14:39:42 -0300

    Added build and deploy docs

---
+++ ecocredit_env_setup.bat
@@ -0,0 +1,23 @@
+set SERVER_PORT=8080
+set DATASOURCE_URL_PROD=
+set DATASOURCE_USER_PROD=
+set DATASOURCE_PASSWORD=
+set EXPIRATION_DURATION_IN_MS=1440000
+set TOKEN_SECRET_KEY=
+set REFRESH_TOKEN_VALIDITY_MS=60400000
+set EMAIL_USERNAME=
+set EMAIL_PASSWORD=
+set EMAIL_HOST=
+set EMAIL_PORT=
+set MAIL_DEBUG_PROD=true
+set FRONTEND_SERVER_URL=
+set STRIPE_PUBLIC_KEY=""
+set STRIPE_SECRET_KEY=""
+set BASE_URL_FRONTEND=""
+set BASE_URL_BACKEND=""
+
+
+echo "Environment variables set"
+echo "You can now run the application from this window using 'mvnw spring-boot:run'"
+echo "Note: If you close the window, the environment variables will be gone"
+echo "Please run this file again if you want to set it again"
