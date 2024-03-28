90afcb57326da1ec4a374b51f78594c80753ff5b
Christin Saji <ch580848@dal.ca>
2024-03-28 16:07:41 -0300

    change description

--- frontend/ecocredit/src/pages/ViewMyBids/ViewMyBids.js
+++ frontend/ecocredit/src/pages/ViewMyBids/ViewMyBids.js
@@ -71,7 +71,7 @@ const ViewMyBids = () => {
                   <dt className="col-sm-5">Current Highest Bid</dt>
                   <dd className="col-sm-7">{bid.highest_bid} CAD</dd>
 
-                  <dt className="col-sm-5">Starting Bid</dt>
+                  <dt className="col-sm-5">My Bid Amount</dt>
                   <dd className="col-sm-7">{bid.bid_amount} CAD</dd>
 
                   <dt className="col-sm-5">Status</dt>
